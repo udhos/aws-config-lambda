0e78ecc3ece0d802e39d166f22dff853e2be4e3d
udhos <udhos>
2019-05-28 17:15:27 -0300

    Config service.

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/udhos/aws-config-lambda
 
 go 1.12
 
-require github.com/aws/aws-lambda-go v1.10.0
+require (
+	github.com/aws/aws-lambda-go v1.10.0
+	github.com/aws/aws-sdk-go-v2 v0.8.0
+)
--- main.go
+++ main.go
@@ -7,10 +7,27 @@ import (
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go-v2/aws/external"
+	"github.com/aws/aws-sdk-go-v2/service/configservice"
 )
 
 type Out struct {
 	Str string
+}
+
+var count int
+
+func getConfig() *configservice.ConfigService {
+
+	cfg, err := external.LoadDefaultAWSConfig()
+	if err != nil {
+		log.Printf("getConfig: %v", err)
+		return nil
+	}
+
+	config := configservice.New(cfg)
+
+	return config
 }
 
 // https://github.com/aws/aws-lambda-go/blob/master/events/README_Config.md
@@ -18,12 +35,21 @@ type Out struct {
 
 func Handler(ctx context.Context, configEvent events.ConfigEvent) (Out, error) {
 
+	count++
+
 	fmt.Printf("fmt: logging from handler: event: %v\n", configEvent)
 	log.Printf("log: logging from handler: event: %v", configEvent)
+
+	log.Printf("count=%d", count)
 
 	fmt.Printf("AWS Config rule: %s\n", configEvent.ConfigRuleName)
 	fmt.Printf("Invoking event JSON: %s\n", configEvent.InvokingEvent)
 	fmt.Printf("Event version: %s\n", configEvent.Version)
+
+	config := getConfig()
+	if config == nil {
+		log.Printf("could not get config service")
+	}
 
 	var err error
 
