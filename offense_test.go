140efd8136795c408794d90d81c9c63a7385f451
udhos <udhos>
2019-06-04 11:47:58 -0300

    Support for testdata.

--- offense_test.go
+++ offense_test.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"encoding/json"
+	"io/ioutil"
+	"path/filepath"
 	"testing"
 )
 
@@ -91,4 +93,51 @@ func TestOffenseJson(t *testing.T) {
 		}
 	}
 
+}
+
+func TestOffenseData(t *testing.T) {
+	root := "testdata"
+	dirItem := filepath.Join(root, "item")     // testdata/item/resource-id   = item
+	dirTarget := filepath.Join(root, "target") // testdata/target/resource-id = target
+	dirResult := filepath.Join(root, "result") // testdata/result/resource-id = annotation = empty "" means no error
+
+	files, errDir := ioutil.ReadDir(dirItem)
+	if errDir != nil {
+		t.Errorf("missing dir testdata: %v", errDir)
+		return
+	}
+
+	for _, f := range files {
+		pathItem := filepath.Join(dirItem, f.Name())
+		bufItem, errItem := ioutil.ReadFile(pathItem)
+		if errItem != nil {
+			t.Errorf("item file %s: %v", f.Name(), errItem)
+			continue
+		}
+		pathTarget := filepath.Join(dirTarget, f.Name())
+		bufTarget, errTarget := ioutil.ReadFile(pathTarget)
+		if errTarget != nil {
+			t.Errorf("target file %s: %v", f.Name(), errTarget)
+			continue
+		}
+		pathResult := filepath.Join(dirResult, f.Name())
+		bufResult, _ := ioutil.ReadFile(pathResult)
+		expectOffense := len(bufResult) == 0
+		im := map[string]interface{}{}
+		if err := json.Unmarshal(bufItem, &im); err != nil {
+			t.Errorf("bad json item %s: %v", f.Name(), err)
+		}
+		tm := map[string]interface{}{}
+		if err := json.Unmarshal(bufTarget, &tm); err != nil {
+			t.Errorf("bad json target %s: %v", f.Name(), err)
+		}
+		dump := false
+		o, annotation := findOffenseMap("", im, tm, dump)
+		if o != expectOffense {
+			t.Errorf("%s offenseExpected=%v offenseFound=%v", f.Name(), expectOffense, o)
+		}
+		if annotation != string(bufResult) {
+			t.Errorf("%s annotationExpected=%s annotationFound=%s", f.Name(), string(bufResult), annotation)
+		}
+	}
 }
